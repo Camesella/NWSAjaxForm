4697f30bbc9762ada0f5858471473e76e6cda5e3
Rodéric Camesella <roderic.camesella@viacesi.fr>
2018-05-24 10:25:03 +0200

    ajout d'un storage

---
+++ testStorage.js
@@ -0,0 +1,14 @@
+function testStorage() {
+    let nom = document.getElementById('nom').value;
+    let prenom = document.getElementById('prenom').value;
+    let mail = document.getElementById('mail').value;
+    let mdp = document.getElementById('mdp').value;
+
+    sessionStorage.setItem("Nom", nom);
+    sessionStorage.setItem("Prenom", prenom);
+    sessionStorage.setItem("email", mail);
+    sessionStorage.setItem("mdp", mdp);
+
+let toto = sessionStorage.getItem("Nom")
+
+}
